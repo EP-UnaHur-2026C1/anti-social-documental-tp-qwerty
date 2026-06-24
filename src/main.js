33b3727f645f2fbcc512d5064122ccb27742cafd
marcos7337 <marcospuned@hotmail.com>
2026-06-23 22:43:13 -0300

    Base del proyecto creada

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 console.log("UnaHur - Anti-Social net");
+
+//require("dotenv").config();
+const dotenv = require("dotenv");
+const express = require("express");
+const connectDB = require("./config/db");
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.send("Anti Social API funcionando");
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+})
